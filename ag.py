75082c969d19bf32aefd3c5e95fce58ae4d3dc72
ian veiga <153085709+ian-veiga@users.noreply.github.com>
2024-08-27 19:43:32 -0300

    Add files via upload

--- ag.py
+++ ag.py
@@ -15,13 +15,27 @@ for l in range(int(nlinhas)):
             delivery_points.append(j)  
 delivery_points.remove('R')
 
-def pop_inicial(map, pop_size):
+def Pop_inicial(points, pop_size):
     population = []
-    permutation = list(permutations(map))
+    permutation =list(permutations(points))
     for i in range(pop_size):
-        population.append(permutation[i])
+        population.append(list(permutation[i]))
     return population
 
+def Fitness(rota, coordinates):
+   rota.insert(0, "R")  # Insere "R" no início
+   rota.append("R")     # Adiciona "R" no final
+   distance = 0
+   for i in range(len(rota) - 1):
+      l_cost = abs(coordinates[rota[i]][0] - coordinates[rota[i+1]][0])
+      c_cost = abs(coordinates[rota[i]][1] - coordinates[rota[i+1]][1])
+      distance += l_cost + c_cost
+   return distance
 
-x = pop_inicial(delivery_points, 24 )
-print (x)
+
+
+
+x = Pop_inicial(delivery_points, 24)
+print(x)
+y = Fitness(x[22], coordinates)
+print(y)
